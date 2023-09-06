166c661d3a10243fe5709a3f84885489289e1824
Attakinsky <joseblanco77@gmail.com>
2023-09-06 10:33:10 -0600

    Ajustando opraciones

--- prueba2.js
+++ prueba2.js
@@ -1,9 +1,10 @@
 // Función suma
 
 function suma(dato1, dato2) {
+    // sumatoria
   return dato1 + dato2;
 }
 
 function resta(dato1, dato2) {
-  return dato1 - dato2;
+  return (dato1 - dato2);
 }
